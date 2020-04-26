ded64ce8b647bffa480b1ee1bafdb6b5a9323533
james-bc <james@blockchain.com>
2020-04-26 15:09:58 +0100

    chore(setup): new setup

--- setup.py
+++ setup.py
@@ -1,23 +1,54 @@
+import sys
+
 import setuptools
+from setuptools.command.test import test as TestCommand
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+class Test(TestCommand):
+    def run_tests(self):
+        import pytest
+
+        errno = pytest.main(["tests/"])
+        sys.exit(errno)
+
+
+REQUIRED = open("requirements.txt").read()
+
 setuptools.setup(
-    name="bcex-ws-SIMON-BC", # Replace with your own username
+    name="bcex",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="simon-bc",
+    author_email="simon@blockchain.com",
+    description="Websocket Client and interface for Blockchain.com Exchange",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    url="https://github.com/simon-bc/bcex",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    cmdclass={"test": Test},
+    install_requires=REQUIRED,
+    tests_require=["pytest"],
+    keywords=[
+        "cryptocurrency",
+        "bitcoin",
+        "btc",
+        "trading",
+        "market feed",
+        "market data",
+        "exchange",
+        "blockchain",
+        "eth",
+        "xlm",
+        "crypto",
+        "market making",
+        "bot",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-
+    python_requires=">=3.6",
 )
