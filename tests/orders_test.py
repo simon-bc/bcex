976df7d7087bb4a2a08ac3e07375a057fa43d22e
James Norton-Brown <44572865+james-bc@users.noreply.github.com>
2020-05-02 16:50:28 +0100

    Add tests (#19)
    * tests: add tests for client and orders
    
    * fix tests
    
    * refactor: change file names for better packages
    
    * refactor: change to put files inside bcex folder

---
+++ tests/orders_test.py
@@ -0,0 +1,109 @@
+import pytest
+from core.orders import Order, OrderSide, OrderType, TimeInForce
+
+
+class TestOrder(object):
+    def test_create_order(self):
+        Order(
+            symbol="BTC-USD",
+            order_type=OrderType.LIMIT,
+            price=1,
+            order_quantity=1,
+            side=OrderSide.SELL,
+            time_in_force=TimeInForce.GTC,
+        )
+        Order(
+            symbol="BTC-USD",
+            order_type=OrderType.MARKET,
+            order_quantity=1,
+            side=OrderSide.SELL,
+        )
+        Order(
+            symbol="BTC-USD",
+            order_type=OrderType.STOP,
+            stop_price=1,
+            order_quantity=1,
+            side=OrderSide.SELL,
+            time_in_force=TimeInForce.GTC,
+        )
+        Order(symbol="BTC-USD", order_type=OrderType.CANCEL, order_id="test")
+        Order(
+            symbol="BTC-USD",
+            order_type=OrderType.LIMIT,
+            price=1,
+            order_quantity=1,
+            side=OrderSide.SELL,
+            time_in_force=TimeInForce.IOC,
+            minimum_quantity=1,
+        )
+        Order(
+            symbol="BTC-USD",
+            order_type=OrderType.LIMIT,
+            price=1,
+            order_quantity=1,
+            side=OrderSide.SELL,
+            time_in_force=TimeInForce.GTC,
+            expiry_date="20201010",
+        )
+
+        with pytest.raises(ValueError):
+            Order(
+                order_type=OrderType.LIMIT,
+                price=1,
+                order_quantity=1,
+                side=OrderSide.SELL,
+                time_in_force=TimeInForce.GTC,
+            )
+        with pytest.raises(ValueError):
+            Order(
+                symbol="BTC-USD",
+                order_type=OrderType.MARKET,
+                price=1,
+                order_quantity=1,
+                side=OrderSide.SELL,
+                time_in_force=TimeInForce.GTC,
+            )
+        with pytest.raises(ValueError):
+            Order(
+                symbol="BTC-USD",
+                order_type=OrderType.MARKET,
+                price=1,
+                order_quantity=1,
+                side=OrderSide.SELL,
+            )
+        with pytest.raises(ValueError):
+            Order(
+                symbol="BTC-USD",
+                order_type=OrderType.LIMIT,
+                price=1,
+                order_quantity=1,
+                side=OrderSide.SELL,
+                time_in_force=TimeInForce.GTD,
+            )
+        with pytest.raises(ValueError):
+            Order(
+                symbol="BTC-USD",
+                order_type=OrderType.STOP,
+                price=1,
+                order_quantity=1,
+                side=OrderSide.SELL,
+                time_in_force=TimeInForce.GTC,
+            )
+        with pytest.raises(ValueError):
+            Order(
+                symbol="BTC-USD",
+                order_type=OrderType.STOP_LIMIT,
+                price=1,
+                order_quantity=1,
+                side=OrderSide.SELL,
+                time_in_force=TimeInForce.GTC,
+            )
+        with pytest.raises(ValueError):
+            Order(
+                symbol="BTC-USD",
+                order_type=OrderType.LIMIT,
+                price=1,
+                order_quantity=1,
+                side=OrderSide.SELL,
+                time_in_force=TimeInForce.IOC,
+            )
