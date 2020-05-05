ec196eda352b41a08d067db8b9a3705870e1d28a
Tony Duriez <tduriez@blockchain.com>
2020-05-05 23:51:41 +0200

    refactor(example) rename strategy and add docstring

--- bcex/examples/hodlr.py
+++ bcex/examples/hodlr.py
@@ -5,7 +5,10 @@ from bcex.core.websocket_client import Environment, Channel
 from bcex.examples.trader import BaseTrader
 
 
-class SimpleBuyTrader(BaseTrader):
+class HODLR(BaseTrader):
+    """Trade which places a market order at regular intervals
+    """
+
     CHANNELS = [
         Channel.HEARTBEAT,
         Channel.TRADING,
@@ -15,7 +18,7 @@ class SimpleBuyTrader(BaseTrader):
     ]
 
     def __init__(
-        self, symbol, quantity=0.05, env=Environment.PROD, buy_intervals=3600,
+        self, symbol, quantity=0.005, env=Environment.PROD, buy_intervals=3600,
     ):
         super().__init__(
             symbol, refresh_rate=buy_intervals, env=env,
@@ -30,5 +33,5 @@ if __name__ == "__main__":
     logging.basicConfig(
         format="%(asctime)s [%(levelname)s] %(message)s", level=logging.DEBUG
     )
-    trader = SimpleBuyTrader(Symbol.BTCUSD, buy_intervals=10, env=Environment.STAGING)
+    trader = HODLR(Symbol.BTCUSD, buy_intervals=60, env=Environment.STAGING)
     trader.run_loop()
